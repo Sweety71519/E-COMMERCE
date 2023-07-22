64dd12d8da8fe67fd7aa9757a3f8df92ccaacb91
Sweety Kumari <sweety.kumari@sw-at.com>
2023-07-22 19:49:31 +0530

    add filte on product

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -11,6 +11,8 @@ export default function Shop() {
     let [mc,setMc]=useState("All")
     let [sc,setSc]=useState("All")
     let [br,setBr]=useState("All")
+    let [min,setMin]=useState(0)
+    let [max,setMax]=useState(5000)
     let params=useParams()
     var stateData = useSelector((state) => state.ProductStateData)
     var AllMaincategoryData = useSelector((state) => state.MaincategoryStateData)
@@ -37,6 +39,36 @@ export default function Shop() {
             setproduct(stateData.slice(1).reverse().filter((item) => item.maincategory === mc && item.subcategory === sc && item.brand === br))
 
 
+
+    }
+    function getPriceInput(e){
+        var {name,value}=e.target 
+        if(name==="min")
+        setMin(value)
+        else
+        setMax(value)
+
+    }
+    function applyPriceFilter(){
+        var p=[]
+        if (mc === "All" && sc === "All" && br === "All")
+            p=stateData.slice(1).reverse()
+        else if (mc !== "All" && sc === "All" && br === "All")
+            p=stateData.slice(1).reverse().filter((item) => item.maincategory === mc)
+        else if (mc === "All" && sc !== "All" && br === "All")
+            p=stateData.slice(1).reverse().filter((item) => item.subcategory === sc)
+        else if (mc === "All" && sc === "All" && br !== "All")
+            p=stateData.slice(1).reverse().filter((item) => item.brand === br)
+        else if (mc !== "All" && sc !== "All" && br === "All")
+            p=stateData.slice(1).reverse().filter((item) => item.maincategory === mc && item.subcategory === sc)
+        else if (mc !== "All" && sc === "All" && br !== "All")
+            p=stateData.slice(1).reverse().filter((item) => item.maincategory === mc && item.brand === br)
+        else if (mc === "All" && sc !== "All" && br !== "All")
+            p=stateData.slice(1).reverse().filter((item) => item.brand === br && item.subcategory === sc)
+        else
+            p=stateData.slice(1).reverse().filter((item) => item.maincategory === mc && item.subcategory === sc && item.brand === br)
+        
+        setproduct(p.filter((item)=>item.finalprice>=min && item.finalprice<=max))
 
     }
 
@@ -139,119 +171,41 @@ export default function Shop() {
                                     </div>
                                 </div>
                                 <div className="sidebar__filter">
-                                    <div className="section-title">
+                                    <div className="card p-3">
+                                    <div className="section-title mb-1">
                                         <h4>Shop by price</h4>
                                     </div>
-                                    <div className="filter-range-wrap">
-                                        <div className="price-range ui-slider ui-corner-all ui-slider-horizontal ui-widget ui-widget-content"
-                                            data-min="33" data-max="99"></div>
                                         <div className="range-slider">
-                                            <div className="price-input">
-                                                <p>Price:</p>
-                                                <input type="text" id="minamount" />
-                                                <input type="text" id="maxamount" />
+                                            <div className="btn-group">
+                                                <div className="mb-3">
+                                                    <label>Min</label>
+                                                <input type="text" name="min" className='form-control w-100' onChange={getPriceInput} value={min} />
+                                                </div>
+                                                <div className="mb-3">
+                                                <label>Max</label>
+                                                <input type="text" name="max" className='form-control w-100' onChange={getPriceInput} value={max}/>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                    <a href="#">Filter</a>
-                                </div>
-                                <div className="sidebar__sizes">
-                                    <div className="section-title">
-                                        <h4>Shop by size</h4>
-                                    </div>
-                                    <div className="size__list">
-                                        <label htmlFor="xxs">
-                                            xxs
-                                            <input type="checkbox" id="xxs" />
-                                            <span className="checkmark"></span>
-                                        </label>
-                                        <label htmlFor="xs">
-                                            xs
-                                            <input type="checkbox" id="xs" />
-                                            <span className="checkmark"></span>
-                                        </label>
-                                        <label htmlFor="xss">
-                                            xs-s
-                                            <input type="checkbox" id="xss" />
-                                            <span className="checkmark"></span>
-                                        </label>
-                                        <label htmlFor="s">
-                                            s
-                                            <input type="checkbox" id="s" />
-                                            <span className="checkmark"></span>
-                                        </label>
-                                        <label htmlFor="m">
-                                            m
-                                            <input type="checkbox" id="m" />
-                                            <span className="checkmark"></span>
-                                        </label>
-                                        <label htmlFor="ml">
-                                            m-l
-                                            <input type="checkbox" id="ml" />
-                                            <span className="checkmark"></span>
-                                        </label>
-                                        <label htmlFor="l">
-                                            l
-                                            <input type="checkbox" id="l" />
-                                            <span className="checkmark"></span>
-                                        </label>
-                                        <label htmlFor="xl">
-                                            xl
-                                            <input type="checkbox" id="xl" />
-                                            <span className="checkmark"></span>
-                                        </label>
-                                    </div>
-                                </div>
-                                <div className="sidebar__color">
-                                    <div className="section-title">
-                                        <h4>Shop by size</h4>
-                                    </div>
-                                    <div className="size__list color__list">
-                                        <label htmlFor="black">
-                                            Blacks
-                                            <input type="checkbox" id="black" />
-                                            <span className="checkmark"></span>
-                                        </label>
-                                        <label htmlFor="whites">
-                                            Whites
-                                            <input type="checkbox" id="whites" />
-                                            <span className="checkmark"></span>
-                                        </label>
-                                        <label htmlFor="reds">
-                                            Reds
-                                            <input type="checkbox" id="reds" />
-                                            <span className="checkmark"></span>
-                                        </label>
-                                        <label htmlFor="greys">
-                                            Greys
-                                            <input type="checkbox" id="greys" />
-                                            <span className="checkmark"></span>
-                                        </label>
-                                        <label htmlFor="blues">
-                                            Blues
-                                            <input type="checkbox" id="blues" />
-                                            <span className="checkmark"></span>
-                                        </label>
-                                        <label htmlFor="beige">
-                                            Beige Tones
-                                            <input type="checkbox" id="beige" />
-                                            <span className="checkmark"></span>
-                                        </label>
-                                        <label htmlFor="greens">
-                                            Greens
-                                            <input type="checkbox" id="greens" />
-                                            <span className="checkmark"></span>
-                                        </label>
-                                        <label htmlFor="yellows">
-                                            Yellows
-                                            <input type="checkbox" id="yellows" />
-                                            <span className="checkmark"></span>
-                                        </label>
-                                    </div>
+                                    <button onClick={applyPriceFilter} className="btn btn-secondary w-100">Filter</button>
                                 </div>
                             </div>
                         </div>
                         <div className="col-lg-9 col-md-9">
+                            <div className=" row mb-3">
+                                <div className="col-md-9 btn-group">
+                                <input type="search" name='search' placeholder='Enter Category,Subcategory,Brand color,Size etc to Search:' className='form-control'></input>
+                                <button className='btn btn-success'>Search</button>
+                                <div className="col-md-3">
+                                    <select name="sortFilter" className='form-control'>
+                                        <option value='1'>Newset</option>
+                                        <option value='2'>Price High to Low</option>
+                                        <option value='3'>Price Low to High</option>
+                                    </select>
+                                </div>
+                                </div>
+                            </div>
                             <div className="row">
 
                                 {
