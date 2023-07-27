c7f5776095f313761cf58d798bcf3dec799198b2
Sweety Kumari <sweety.kumari@sw-at.com>
2023-07-28 00:27:41 +0530

    commit

--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -51,98 +51,118 @@ export default function SingleProduct() {
             {/*  Breadcrumb End  */}
 
             {/*  Product Details Section Begin  */}
-            <section className="product-details spad">
+            <section className="product-details my-2 p-2">
                 <div className="container">
                     <div className="row">
-                        <div className="col-lg-6">
-                            <div className="product__details__pic">
-                                <div className="product__details__pic__left product__thumb nice-scroll">
-                                    <a className="pt active" href="#product-1">
-                                        <img src={`/images/${product.pic1}`} height="120px" width="100%" alt="" />
-                                    </a>
-                                    <a className="pt" href="#product-2">
-                                        <img src={`/images/${product.pic2}`} height="120px" width="100%" alt="" />
-                                    </a>
-                                    <a className="pt" href="#product-3">
-                                        <img src={`/images/${product.pic3}`} height="120px" width="100%" alt="" />
-                                    </a>
-                                    <a className="pt" href="#product-4">
-                                        <img src={`/images/${product.pic4}`} height="120px" width="100%" alt="" />
-                                    </a>
-                                </div>
-                                <div className="product__details__slider__content">
-                                    <OwlCarousel className='product__details__pic__slider owl-theme' loop margin={10} items={1} nav>
-                                        <img data-hash="product-1" className="product__big__img" src={`/images/${product.pic1}`} height="540px" width="100%" alt="" />
-                                        <img data-hash="product-2" className="product__big__img" src={`/images/${product.pic2}`} height="540px" width="100%" alt="" />
-                                        <img data-hash="product-3" className="product__big__img" src={`/images/${product.pic3}`} height="540px" width="100%" alt="" />
-                                        <img data-hash="product-4" className="product__big__img" src={`/images/${product.pic4}`} height="540px" width="100%" alt="" />
-                                    </OwlCarousel>
+                        <div className="col-lg-7">
+                            <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+                                <div className="row">
+                                    <div className="col-3 d-flex flex-column justify-content-between">
+                                        <img src={`/images/${product.pic1}`} alt="" height="100px" width="100px" data-target="#carouselExampleControls" data-slide-to="0" className="active" />
+                                        <img src={`/images/${product.pic2}`} alt="" height="100px" width="100px" data-target="#carouselExampleControls" data-slide-to="1" />
+                                        <img src={`/images/${product.pic3}`} alt="" height="100px" width="100px" data-target="#carouselExampleControls" data-slide-to="2" />
+                                        <img src={`/images/${product.pic4}`} alt="" height="100px" width="100px" data-target="#carouselExampleControls" data-slide-to="3" />
+                                    </div>
+                                    <div className="col-9">
+                                        <div className="carousel-inner">
+                                            <div className="carousel-item active">
+                                                <img className="d-block w-100" src={`/images/${product.pic1}`} height="450px" alt="First slide" />
+                                            </div>
+                                            <div className="carousel-item">
+                                                <img className="d-block w-100" src={`/images/${product.pic2}`} height="450px" alt="Second slide" />
+                                            </div>
+                                            <div className="carousel-item">
+                                                <img className="d-block w-100" src={`/images/${product.pic3}`} height="450px" alt="Third slide" />
+                                            </div>
+                                            <div className="carousel-item">
+                                                <img className="d-block w-100" src={`/images/${product.pic4}`} height="450px" alt="Third slide" />
+                                            </div>
+                                        </div>
+                                        <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                            <span className="sr-only">Previous</span>
+                                        </a>
+                                        <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                            <span className="sr-only">Next</span>
+                                        </a>
+
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <div className="col-lg-6">
+                        <div className="col-lg-5">
                             <div className="product__details__text">
-                                <h3>{product.name} <span>Category: {product.maincategory}/{product.subcategory}/{product.brand}</span></h3>
-                                <div className="product__details__price">&#8377;{product.finalprice}<span>&#8377;{product.baseprice}</span><sup className='text-success'>{product.discount}%Off</sup></div>
-                                <p>{product.description}</p>
-                                <div className="btn-group w-25 mb-4" style={{ width: "30%" }}>
-                                    <button className='btn btn-success w-25 px-2' onClick={() => {
-                                        if (qty > 1)
-                                            setQty(qty - 1)
-                                    }}><i className='fa fa-minus'></i></button>
-                                    <p className='px-3 mt-1 fs-1 w-50 text-center' style={{ fontSize: "18px", fontWeight: "bold" }}>{qty}</p>
-                                    <button className='btn btn-success w-25 px-2' onClick={() => setQty(qty + 1)} ><i className='fa fa-plus'></i></button>
-                                </div>
-                                <br />
-                                <div className="btn-group">
-                                    <button className='btn btn-primary'><span className="icon_bag_alt"></span> Add to cart</button>
-                                    <button className='btn btn-success'><span className="icon_heart_alt"></span>Add to wishlist</button>
-                                </div>
-                                <div className="product__details__widget">
-                                    <ul>
-                                        <li>
-                                            <span>Availability:</span>
-                                            <div className="stock__checkbox">
-                                                {product.stock}
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <span>Color:</span>
-                                            {product.color}
-                                        </li>
-                                        <li>
-                                            <span>Available size:</span>
-                                            <div className="size__btn">
-                                                {product.size}
-                                            </div>
-                                        </li>
-                                    </ul>
-                                </div>
+                                <div className="table-responsible">
+                                    <table className='table table-bordered table-stripped table-hover'>
+                                        <tr>
+                                            <th colSpan={2}>{product.name}</th>
+                                        </tr>
+                                        <tr>
+                                            <th>Category</th>
+                                            <td>{product.maincategory}/{product.subcategory}/{product.brand}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Color</td>
+                                            <td>{product.color}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Size</td>
+                                            <td>{product.size}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Price</td>
+                                            <td><del className='text-danger'>&#8377;{product.baseprice}</del> &#8377;{product.finalprice}<sup className='text-success'>{product.discount}%Off</sup></td>
+                                        </tr>
+                                        <tr>
+                                            <td colSpan={2} className="text-justify">{product.description}</td>
+                                        </tr>
+                                        <tr>
+                                            <td colSpan={2} className='d-flex justify-content-between'>
+                                                <button className='btn btn-secondary btn-sm'><i className='fa fa-plus btn'></i></button>
+                                                <h4>{qty}</h4>
+                                                <button className='btn btn-secondary btn-sm'><i className='fa fa-minus btn'></i></button>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td colSpan={2}>
+                                                <div className='btn-group w-100'>
+                                                    <button className='w-50 btn btn-success btn-sm'><i className='fa fa-shopping-cart'></i>  Add to Cart</button>
+                                                    <button className='w-50 btn btn-secondary btn-sm'><i className='fa fa-heart'></i> Add to Wishlist</button>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </div>                        
                             </div>
                         </div>
                     </div>
                     {
                         <>
 
-                        relatedproduct.length ?
+                            relatedproduct.length ?
                             <div className="col-lg-12 text-center">
                                 <div className="related__title">
                                     <h5>RELATED PRODUCTS</h5>
                                 </div>
                             </div>
-                            <OwlCarousel loop navText={(1)} margin={10} items={4} style={{ justifyContent: "center", margin: "200px 0" }}>
-                                <div className="mt-1">
-                                    <div className="product__item">
-                                        <div className="product__item__pic set-bg" style={{ backgroundImage: 'url("/img/product/related/rp-1.jpg")' }}>
-                                            <div className="label new">New</div>
-                                            <ul className="product__hover">
-                                                <li><Link to="/img/product/related/rp-1.jpg" className="image-popup"><span className="arrow_expand"></span></Link></li>
-                                                <li><Link to="#"><span className="icon_heart_alt"></span></Link></li>
-                                                <li><Link to="#"><span className="icon_bag_alt"></span></Link></li>
-                                            </ul>
+                            <OwlCarousel loop navText={(1)} margin={10} items={4} style={{ justifyContent: "center", margin: "20px 0" }}>
+                                {
+                                    relatedproduct.map((item, index) => {
+                                        return <div key={index} className="product__item">
+                                            <Link to={`/single-product/${item.id}`}>
+                                                <div className="product__item__pic set-bg" style={{ backgroundImage: `url("/images/${item.pic1}")` }}>
+                                                    <div className="label new">New</div>
+                                                </div>
+                                            </Link>
+                                            <div className="product__item__text">
+                                                <h6><Link to={`/single-product/${item.id}`}>{item.name}</Link></h6>
+                                                <div className="product__price"><del className='text-danger'>&#8377;{item.baseprice}</del>&#8377;{item.finalprice}<span className='text-success text-decoration-none'> {item.discount}% Off</span></div>
+                                            </div>
                                         </div>
-                                    </div>
-                                </div>
+
+                                    })
+                                }
                             </OwlCarousel>:""
                         </>
 
